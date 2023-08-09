c3687fb78b706e9b3c0017d0ace99aec74dd1221
Breno Rocha <brenopenarocha2@gmail.com>
2023-08-09 18:31:21 -0300

    change size function

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
 const screen = document.querySelector('.screen');
+const sizeValue = document.getElementById('size-value');
+const slider = document.getElementById('slider');
+const black = document.getElementById('black-bt');
+const white = document.getElementById('white-bt');
+const rainbow = document.getElementById('rainbow-bt');
+const erase = document.getElementById('erase-bt');
+
 
 function generateScreen() {
-    for(i = 0; i < 64; i++) {
+    for(let i = 0; i < 64; i++) {
         let div = document.createElement('div');
         div.classList.add('cell');
+        screen.appendChild(div);
+    }
+}
 
+function removeAllChildNodes(parent) {
+    while(parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function changeSize(value) {
+    sizeValue.textContent = value;
+
+    removeAllChildNodes(screen);
+    screen.setAttribute('style', `grid-template-columns: repeat(${value}, 1fr); grid-template-rows: repeat(${value}, 1fr)`);
+
+    for(let i = 0; i < value*value; i++) {
+        let div = document.createElement('div');
+        div.classList.add('cell');
         screen.appendChild(div);
     }
 }
 
 
+slider.oninput = event => changeSize(event.target.value);
 
 
 generateScreen();
